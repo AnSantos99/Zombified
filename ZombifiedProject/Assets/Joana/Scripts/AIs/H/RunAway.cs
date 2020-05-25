8a0e19dbc4ddcce63802c7dd163539c92e8bd0ca
Joana Silva <joana.f.silva.99@gmail.com>
2020-05-25 17:58:01 +0100

    Add Wandering, Run away, Waypoints and Movemen towards closest entety

---
+++ ZombifiedProject/Assets/Joana/Scripts/AIs/H/RunAway.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RunAway : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    [SerializeField]private GameObject Player;
+
+    [SerializeField]private float EnemyDistanceRun = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+
+        if (distance < EnemyDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+            Vector3 newPos = transform.position + dirToPlayer;
+
+            agent.SetDestination(newPos);
+        }
+    }
+}
---
+++ ZombifiedProject/Assets/Joana/Scripts/PlayerMov.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    private string horizontalInputName = "Horizontal";
+    private string verticalInputName = "Vertical";
+
+    [SerializeField] private float walkSpeed, runSpeed;
+    [SerializeField] private float runBuildUpSpeed;
+    [SerializeField] private KeyCode runKey;
+
+    private PatrollAgent[] Lpa;
+
+    private float movementSpeed;
+
+    private bool walking;
+
+    private CharacterController charController;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+
+    [SerializeField] private float crouchHeight;
+
+    private bool isJumping;
+    public bool hidden;
+
+    public int Lives = 3;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+        walking = false;
+    }
+
+    private void Start()
+    {
+        Lpa = GameObject.FindObjectsOfType<PatrollAgent>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+
+        Crouch();
+
+        //GameOver();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName);
+        float vertInput = Input.GetAxis(verticalInputName);
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+
+        SetMovementSpeed();
+        JumpInput();
+    }
+
+    private void SetMovementSpeed()
+    {
+        if (Input.GetKey(runKey))
+        {
+            walking = false;
+            movementSpeed = Mathf.Lerp(movementSpeed, runSpeed, Time.deltaTime * runBuildUpSpeed);
+        }
+
+        else
+        {
+            walking = true;
+            movementSpeed = Mathf.Clamp(movementSpeed, walkSpeed, Time.deltaTime * runBuildUpSpeed);
+        }
+    }
+
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    void Crouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) || hidden == true)
+        {
+            charController.height = crouchHeight;
+            movementSpeed = 0.5f;
+            isJumping = true;
+        }
+
+        else
+        {
+            charController.height = 2;
+            isJumping = false;
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 45.0f;
+        isJumping = false;
+    }
+
+    public event System.Action <bool> onHidden;
+
+    public void teleport(Vector3 teleportPoint) //Spawnpoint
+    {
+        charController.enabled = false;
+        transform.position = teleportPoint;
+        charController.enabled = true;
+    }
+}
